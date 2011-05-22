aa7cedda2e105e77262e04a6a4e9db4943461a36
Aldiantoro Nugroho <kriwil@gmail.com>
2011-05-22 12:37:55 +0800

    Template context processors

--- settings.py
+++ settings.py
@@ -18,5 +18,15 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	PROJECT_PATH + '/templates',
+    PROJECT_PATH + '/templates',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
 )
