43ac409dcbc216eedc7a28f4100eb2c524a12d10
Aldiantoro Nugroho <kriwil@gmail.com>
2011-08-31 19:29:15 +0700

    Added app template.

---
+++ apps/tests.py
@@ -0,0 +1,3 @@
+from test_set.authentication import AuthenticatedTest, UnauthenticatedTest
+from test_set.authorization import AuthorizedTest, UnauthorizedTest
+from test_set.manipulation import AddTest, EditTest, DeleteTest
