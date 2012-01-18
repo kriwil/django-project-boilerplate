eebae9c63a12b764372f977520af9df7a3c877e7
Aldiantoro Nugroho <kriwil@gmail.com>
2012-01-19 07:22:41 +0800

    Updated settings and local_settings.

---
+++ local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
